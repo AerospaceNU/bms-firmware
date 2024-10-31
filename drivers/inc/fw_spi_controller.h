174b9934c2fc4ede17ae6a905686ec60abba984c
Ephram Cola Jacquin <colajacquin.e@northeastern.edu>
2024-10-31 19:56:59 -0400

    feat(spi): add base starter code

---
+++ drivers/inc/fw_spi_controller.h
@@ -0,0 +1,6 @@
+#ifndef FW_SPI_CONTROLLER_H
+#define FW_SPI_CONTROLLER_H
+
+int spi_controller();
+
+#endif // FW_SPI_CONTROLLER_H
---
+++ drivers/src/fw_spi_controller.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+
+int spi_controller() {
+  char *buffer = calloc(256, sizeof (char));
+    
+  while(fgets(buffer, 256, stdin) != NULL) {
+
+  }
+}
