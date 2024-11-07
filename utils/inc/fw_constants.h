dda0aa45935e2498232ea6090efa53d9b479bb08
Ephram Cola Jacquin <colajacquin.e@northeastern.edu>
2024-11-07 18:18:07 -0500

    feat: add spi pico pins

--- utils/inc/fw_constants.h
+++ utils/inc/fw_constants.h
@@ -1,6 +1,18 @@
 #ifndef FW_CONSTANTS_H
 #define FW_CONSTANTS_H
 
+//*************************************************************************************************
+// SPI Defines
+//*************************************************************************************************
+
+// SPI Pinouts
+#define PIN_SDI 16
+#define PIN_CSB 17
+#define PIN_SCK 18
+#define PIN_SDO 19
+
 #define SPI_MAX_INPUT_SIZE 256 // TODO: figure out max SPI input
+
+//*************************************************************************************************
 
 #endif // FW_CONSTANTS_H
