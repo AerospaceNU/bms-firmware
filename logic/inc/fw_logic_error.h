9056abcf042fa7de8671f0076438359aeb8b5218
Ephram Cola Jacquin <colajacquin.e@northeastern.edu>
2024-11-07 20:00:28 -0500

    feat(errors): create custom error definitions

---
+++ logic/inc/fw_logic_error.h
@@ -0,0 +1,22 @@
+//*************************************************************************************************
+//  Module                  :   fw_logic_error.h
+//  Revision                :   0.1
+//  Description             :   Custom error declarations and handler functions
+//*************************************************************************************************
+#ifndef _FW_LOGIC_ERROR_
+#define _FW_LOGIC_ERROR_
+
+#define E_SPI(x)        (100+x)
+
+#define E_FW(x)         (1000 + x)
+#define E_PICO(x)       E_FW(100 + x)
+
+/*************************************************************************************************/
+/* SPI Errors */
+#define E_SPI_INIT      E_SPI(1)
+
+/*************************************************************************************************/
+/* Raspberry Pico System Errors */
+#define E_PICO_IO       E_PICO(1)
+
+#endif // _FW_LOGIC_ERROR_
