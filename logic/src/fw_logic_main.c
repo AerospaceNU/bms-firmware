f56bb49fc7c4a3ecc9e152d372033e7175d1b014
Ephram Cola Jacquin <ecolajacquin@linevisioninc.com>
2025-02-14 00:50:37 +0000

    test(main): add basic main logic

--- logic/src/fw_logic_main.c
+++ logic/src/fw_logic_main.c
@@ -1,9 +1,24 @@
+/* Local Includes */
 #include "pico/stdlib.h"
+#include "fw_util_logging.h"
 
 int main() {
-  if (stdio_init_all() == false) {
-    // print_error E_PICO_IO
+  int err = 0;
+
+  err = fw_logic_init();
+
+  while(1) {
+    logMessage(__FILE__, __LINE__, "Hello Message!");
+    sleep_ms(2000);
+    logDebug(__FILE__, __LINE__, "Hello Debug!!");
+    sleep_ms(5000);
   }
 
-  return 0;
+  return err;
+}
+
+int fw_logic_init() {
+  if (stdio_init_all() == false) {
+    logError(__FILE__, __LINE__, "Could not initialize stdio");
+  }
 }
