b11d3698ae412fde40d8782acca89352a2a570da
Ephram Cola Jacquin <colajacquin.e@northeastern.edu>
2025-05-01 22:52:11 +0000

    feat: add spi testing loop

--- logic/src/fw_logic_main.c
+++ logic/src/fw_logic_main.c
@@ -13,6 +13,10 @@ int fw_logic_init() {
   if (stdio_init_all() == false) {
     LOG_ERROR("Could not initialize stdio");
   }
+
+  int ec = fw_spi_controller_init(spi0, MEGAHZ, PIN_SDI, PIN_CSB, PIN_SCK, PIN_SDO);
+
+  return ec;
 }
 
 int main() {
@@ -20,11 +24,17 @@ int main() {
 
   err = fw_logic_init();
 
+  sleep_ms(5000);
+  LOG_MESSAGE("Hello Message!");
+
+  uint8_t spi_in_buf[SPI_MAX_INPUT_SIZE];
+
   while(1) {
-    LOG_MESSAGE("Hello Message!");
-    sleep_ms(2000);
-    LOG_DEBUG("Hello Debug!!");
-    sleep_ms(5000);
+    fw_spi_controller_r_bl(spi0, NULL, spi_in_buf, SPI_MAX_INPUT_SIZE);
+    LOG_MESSAGE("SPI IN:");
+    for (int i = 0; i < SPI_MAX_INPUT_SIZE; i++) {
+      printf("%02X ", spi_in_buf[i]);
+    }
   }
 
   return err;
