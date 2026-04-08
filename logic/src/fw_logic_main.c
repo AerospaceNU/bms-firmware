695834b1e62affdabc1e29ec1444d782e2287805
Ethan Ostroff <ethanostroff1@gmail.com>
2026-04-07 20:07:14 -0400

    feat: initialize MCP4726 DAC and configure I2C settings in fw_logic_init

--- logic/src/fw_logic_main.c
+++ logic/src/fw_logic_main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 /* Local Includes */
+#include "hardware/i2c.h"
 #include "pico/stdlib.h"
 
 #include "utils/inc/fw_constants.h"
@@ -9,7 +10,16 @@
 #include "utils/inc/fw_util_logging.h"
 #include "utils/inc/fw_globals.h"
 #include "logic/inc/fw_logic_error.h"
+#include "drivers/inc/fw_driver_bq76972.h"
+#include "drivers/inc/fw_driver_mcp4726.h"
 #include "drivers/inc/fw_driver_smbus.h"
+
+#define FW_MCP4726_PORT i2c1
+#define FW_MCP4726_SDA 14
+#define FW_MCP4726_SCL 15
+#define FW_MCP4726_ADDR FW_MCP4726_ADDR_DEFAULT
+
+static fw_mcp4726_t g_mcp4726;
 
 int fw_logic_init()
 {
@@ -22,6 +32,40 @@ int fw_logic_init()
   if (fw_driver_smbus_init(0x0B) != 0)
   {
     LOG_ERROR("Could not initialize I2C slave");
+    return E_FW_LOGIC_INIT;
+  }
+
+  fw_bq76972_init();
+
+  if (fw_bq76972_configure_thermistors_default() != FW_BQ76972_SUCCESS)
+  {
+    LOG_ERROR("Could not configure BQ76972 thermistors");
+    return E_BQ76972(1);
+  }
+
+  if (fw_bq76972_configure_user_amps(FW_USER_AMPS_1_MA) != FW_BQ76972_SUCCESS)
+  {
+    LOG_ERROR("Could not configure BQ76972 current scaling");
+    return E_BQ76972(2);
+  }
+
+  fw_driver_smbus_refresh();
+
+  i2c_init(FW_MCP4726_PORT, 100 * 1000);
+  gpio_set_function(FW_MCP4726_SDA, GPIO_FUNC_I2C);
+  gpio_set_function(FW_MCP4726_SCL, GPIO_FUNC_I2C);
+  gpio_pull_up(FW_MCP4726_SDA);
+  gpio_pull_up(FW_MCP4726_SCL);
+
+  if (!fw_driver_mcp4726_init(&g_mcp4726, FW_MCP4726_PORT, FW_MCP4726_ADDR, FW_MCP4726_VREF_DEFAULT))
+  {
+    LOG_ERROR("Could not initialize MCP4726 DAC");
+    return E_FW_LOGIC_INIT;
+  }
+
+  if (!fw_driver_mcp4726_set_voltage(&g_mcp4726, 0.0f))
+  {
+    LOG_ERROR("Could not set MCP4726 DAC output");
     return E_FW_LOGIC_INIT;
   }
 
@@ -52,6 +96,8 @@ int main()
 
   while (1)
   {
+    fw_driver_smbus_refresh();
+    sleep_ms(50);
     tight_loop_contents();
   }
 
